a5d9dbd3cb066e9a92f39b5481c84969c6264aac
aneaiyeapiiwarai-commits <aneaiyeapiiwarai@gmail.com>
2025-09-15 17:22:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'LB0G2SCD#AhyRnTP8hkb0y19Pvd906XUJeVg3Zr2dce_XGV0Rb04' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
